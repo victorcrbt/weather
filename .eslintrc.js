a8f5a945a6091a8d11cdb1991f64fc0b819ac345
Victor Batalha <madivcb@gmail.com>
2021-07-11 21:09:56 -0300

    chore: setup path aliases

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,38 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'import',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx'] }],
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'error',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+    },
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ts',
+          '.tsx',
+          '.js',
+          '.jsx',
+          '.json',
+        ],
+        alias: {
+          '@components': './src/components',
+          '@configs': './src/configs',
+          '@helpers': './src/helpers',
+          '@screens': './src/screens',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 };
