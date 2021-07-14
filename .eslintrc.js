9218132a7114937bca62c18552d4ed3aa2802b9c
Victor Batalha <madivcb@gmail.com>
2021-07-13 22:55:54 -0300

    chore: disable camelcase rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-use-before-define': ['error'],
     '@typescript-eslint/no-unused-vars': 'error',
+    camelcase: 'off',
     'react/prop-types': 'off',
     'no-use-before-define': 'off',
     'no-unused-vars': 'off',
