3ee53d79bd33ce97c176a6ef21e33a98f887b2d0
Victor Batalha <madivcb@gmail.com>
2021-07-14 22:15:52 -0300

    chore: created refresh button

---
+++ src/components/Controls/RefreshButton/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.Pressable`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+  width: 48px;
+  height: 48px;
+
+  border-radius: 24px;
+
+  justify-content: center;
+  align-items: center;
+`;
