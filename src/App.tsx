87b1274efef17f32f8dd51435af7767c35d039b6
Victor Batalha <madivcb@gmail.com>
2021-07-14 22:15:04 -0300

    chore: add flash message container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import FlashMessage from 'react-native-flash-message';
 
 import { Routes } from './routes';
 
 const App = () => (
-  <Routes />
+  <>
+    <Routes />
+    <FlashMessage position="top" />
+  </>
 );
 
 export default App;
